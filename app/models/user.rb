3c0c2db3739f91e27da309729b0e884db98d964e
Jorge Hristho Precich Serna <precichysim@gmail.com>
2019-04-06 15:06:31 -0600

    fix: length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :curp, :gender, presence: true
   validates :curp, uniqueness: true
-  validates :curp, length: 18
-  validates :gender, length: 1
+  validates :curp, length: { is: 18 }
+  validates :gender, length: { is: 1 }
 end
