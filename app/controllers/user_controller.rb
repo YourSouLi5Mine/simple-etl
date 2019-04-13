a5939c3dc0c0e3b218ea6f06e86913d97849bece
Jorge Hristho Precich Serna <precichysim@gmail.com>
2019-04-13 14:40:28 -0500

    feat: states extraction from curp

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -18,10 +18,49 @@ class UserController < ApplicationController
       if row[1] === 'X'
         row[1] = row[0][10]
       end
+      row[2] = states[row[0][11..12].to_sym]
     end
   end
 
   def upload
-    @rows.map { |row| User.create(curp: row[0], gender: row[1]) }
+    @rows.map { |row| User.create(curp: row[0], gender: row[1], state: row[2]) }
+  end
+
+  def states
+    state = {
+      'AS': 'AGUASCALIENTES',
+      'BC': 'BAJA CALIFORNIA',
+      'BS': 'BAJA CALIFORNIA SUR',
+      'CC': 'CAMPECHE',
+      'CL': 'COAHUILA',
+      'CM': 'COLIMA',
+      'CS': 'CHIAPAS',
+      'CH': 'CHIHUAHUA',
+      'DF': 'DISTRITO FEDERAL',
+      'DG': 'DURANGO',
+      'GT': 'GUANAJUATO',
+      'GR': 'GUERRERO',
+      'HG': 'HIDALGO',
+      'JC': 'JALISCO',
+      'MC': 'MEXICO',
+      'MN': 'MICHOACAN',
+      'MS': 'MORELOS',
+      'NT': 'NAYARIT',
+      'NL': 'NUEVO LEON',
+      'OC': 'OAXACA',
+      'PL': 'PUEBLA',
+      'QT': 'QUERETARO',
+      'QR': 'QUINTANA ROO',
+      'SP': 'SAN LUIS POTOSI',
+      'SL': 'SINALOA',
+      'SR': 'SONORA',
+      'TC': 'TABASCO',
+      'TS': 'TAMAULIPAS',
+      'TL': 'TLAXCALA',
+      'VZ': 'VERACRUZ',
+      'YN': 'YUCATAN',
+      'ZS': 'ZACATECAS',
+      'NE': 'NACIDO EN EL EXTRANJERO'
+    }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_06_190802) do
+ActiveRecord::Schema.define(version: 2019_04_13_193413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_04_06_190802) do
     t.string "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "state"
   end
 
 end
